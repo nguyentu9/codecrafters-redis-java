6ee3b1fbb4ea300cd9316cfe70cdc0bf86419498
nguyentu9 <huynhthanhtoan517@gmail.com>
2024-11-09 08:42:41 +0700

    respond to multiple PINGs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,8 +14,9 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write("+PONG\r\n".getBytes());
+
+            process(clientSocket);
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
@@ -27,6 +27,23 @@ public class Main {
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
             }
+        }
+    }
+
+    private static void process(Socket clientSocket) throws IOException {
+        try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        ) {
+            String content;
+            while ((content = reader.readLine()) != null) {
+                System.out.println("::" + content);
+                if ("ping".equalsIgnoreCase(content)) {
+                    writer.write("+PONG\r\n");
+                    writer.flush();
+                }
+            }
+
         }
     }
 }
