5eaf48706ee6f8d07296ad2abee124429e77a81b
nguyentu9 <huynhthanhtoan517@gmail.com>
2024-11-09 09:03:02 +0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,48 +2,59 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+public class Main extends Thread {
+
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
-        try {
-            serverSocket = new ServerSocket(port);
+        try (
+                ServerSocket serverSocket = new ServerSocket(port);
+        ) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
-            clientSocket = serverSocket.accept();
 
-            process(clientSocket);
+            while (true) {
+                new ClientHandler(serverSocket.accept()).start();
+            }
 
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
+        }
+    }
+
+    private static class ClientHandler extends Thread {
+        Socket clientSocket = null;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        public void run() {
+            try (
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            ) {
+                String content;
+                while ((content = reader.readLine()) != null) {
+                    System.out.println("::" + content);
+                    if ("ping".equalsIgnoreCase(content)) {
+                        writer.write("+PONG\r\n");
+                        writer.flush();
+                    }
                 }
+
             } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
-        }
-    }
-
-    private static void process(Socket clientSocket) throws IOException {
-        try (
-                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        ) {
-            String content;
-            while ((content = reader.readLine()) != null) {
-                System.out.println("::" + content);
-                if ("ping".equalsIgnoreCase(content)) {
-                    writer.write("+PONG\r\n");
-                    writer.flush();
+                try {
+                    if (clientSocket != null) {
+                        clientSocket.close();
+                    }
+                } catch (IOException _) {
                 }
+                throw new RuntimeException(e);
             }
 
         }
     }
+
 }
