17f73044b63f3e850dc3715f41c055e55d1196bc
nguyentu9 <huynhthanhtoan517@gmail.com>
2024-11-09 10:20:57 +0700

    Implement the SET & GET commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Main extends Thread {
+    public static final String OK = "+OK\r\n";
+    public static final String NULL_BULK_STRING = "$-1\r\n";
+    public static HashMap<String, String> hashMap = new HashMap<>();
 
     public static void main(String[] args) {
         int port = 6379;
@@ -39,19 +43,22 @@ public class Main extends Thread {
                 while ((content = reader.readLine()) != null) {
                     System.out.println("::" + content);
                     if ("ping".equalsIgnoreCase(content)) {
-                        writer.write("+PONG\r\n");
-                        writer.flush();
+                        pingHandler(writer);
                     }
 
                     if (content.toLowerCase().startsWith("echo")) {
-                        reader.readLine();
-                        content = reader.readLine();
-                        System.out.println("::" + content);
-
-                        String str = String.format("$%d\r\n%s\r\n", content.length(), content);
-                        writer.write(str);
-                        writer.flush();
+                        echoHandler(reader, writer);
                     }
+
+                    if (content.toLowerCase().startsWith("set")) {
+                        setHandler(reader, writer);
+                    }
+
+                    if (content.toLowerCase().startsWith("get")) {
+                        getHandler(reader, writer);
+                    }
+
+
                 }
 
             } catch (IOException e) {
@@ -64,6 +71,56 @@ public class Main extends Thread {
                 throw new RuntimeException(e);
             }
 
+        }
+
+        private void getHandler(BufferedReader reader, BufferedWriter writer) throws IOException {
+            reader.readLine();
+            String key = reader.readLine();
+            System.out.println("::" + key);
+
+            String value = hashMap.get(key);
+            System.out.println("::" + value);
+
+            if ("NULL".equals(value)) {
+                writer.write(NULL_BULK_STRING);
+                writer.flush();
+                return;
+            }
+
+            writer.write(getFormat(value));
+            writer.flush();
+        }
+
+        private void setHandler(BufferedReader reader, BufferedWriter writer) throws IOException {
+            reader.readLine();
+            String key = reader.readLine();
+            System.out.println("::" + key);
+
+            reader.readLine();
+            String value = reader.readLine();
+            System.out.println("::" + value);
+
+            hashMap.put(key, value);
+            writer.write(OK);
+            writer.flush();
+        }
+
+        private static void echoHandler(BufferedReader reader, BufferedWriter writer) throws IOException {
+            reader.readLine();
+            String content = reader.readLine();
+            System.out.println("::" + content);
+
+            writer.write(getFormat(content));
+            writer.flush();
+        }
+
+        private static void pingHandler(BufferedWriter writer) throws IOException {
+            writer.write("+PONG\r\n");
+            writer.flush();
+        }
+
+        private static String getFormat(String content) {
+            return String.format("$%d\r\n%s\r\n", content.length(), content);
         }
     }
 
